27dfa167a3c717cd895baab213e4244eb42208bb
tracer-ics2025 <tracer@njuics.org>
2026-02-02 01:31:59 +0800

    >  compile NEMU 114514 E_Power Linux ArchLinuxOS 6.18.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Sat, 24 Jan 2026 00:47:39 +0000 x86_64 GNU/Linux  01:31:59 up  9:02,  1 user,  load average: 0.50, 0.40, 0.44

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -23,7 +23,7 @@ const char *regs[] = {"$0", "ra", "sp",  "gp",  "tp", "t0", "t1", "t2",
 
 void isa_reg_display() {
   for (int i = 0; i < sizeof(regs) / sizeof(regs[0]); ++i) {
-    printf("%s = 0x%08x\n", regs[i], cpu.gpr[i]);
+    printf("%s = 0x%x\n", regs[i], cpu.gpr[i]);
   }
 }
 
