7b7c2fb2b05ad2e1d208dea6323344d27bf5f6d2
tracer-ics2025 <tracer@njuics.org>
2026-02-02 00:43:15 +0800

    >  compile NEMU 114514 E_Power Linux ArchLinuxOS 6.18.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Sat, 24 Jan 2026 00:47:39 +0000 x86_64 GNU/Linux  00:43:15 up  8:13,  1 user,  load average: 1.56, 1.21, 0.71

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -80,9 +80,9 @@ static void exec_once(Decode *s, vaddr_t pc) {
 }
 
 static void execute(uint64_t n) {
-  printf("executed\n");
   Decode s;
   for (; n > 0; n--) {
+    printf("executed\n");
     exec_once(&s, cpu.pc);
     g_nr_guest_inst++;
     trace_and_difftest(&s, cpu.pc);
