1ff3f14d55fc160273db9787d52793bb81cc362a
tracer-ics2025 <tracer@njuics.org>
2026-02-04 03:11:36 +0800

    >  compile NEMU 114514 E_Power Linux ArchLinuxOS 6.18.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Sat, 24 Jan 2026 00:47:39 +0000 x86_64 GNU/Linux  03:11:36 up 11:34,  1 user,  load average: 0.11, 0.16, 0.17

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -62,7 +62,10 @@ static int cmd_si(char *args) {
     return 0;
   }
   uint64_t steps = (uint64_t)(strtol(args, NULL, 10));
-  cpu_exec(steps ? steps : 1);
+  if (steps)
+    cpu_exec(steps);
+  else
+    printf("Invalid arguments\n");
   return 0;
 }
 
